aed07d805b6764c812e266c0885913524a0e72c7
Ronald <Mail@rphilipsen.nl>
2018-09-20 23:40:03 +0200

    fixed parralellism

--- Web Scraping/Script.R
+++ Web Scraping/Script.R
@@ -1,9 +1,13 @@
 library(rvest)
 library(magrittr)
 library(stringr)
-
+library(doParallel)
+library(foreach)
+library(dplyr)
 
 parseMovies <- function(movies) {
+    options(stringsAsFactors = FALSE)
+
     titles <- c()
     years <- c()
     scores <- c()
@@ -14,58 +18,72 @@ parseMovies <- function(movies) {
 
     for (movie in movies) {
         titles <- append(titles, (html_node(movie, ".lister-item-header a") %>% html_text))
-
         year = html_node(movie, ".lister-item-year") %>% html_text
-        years <- append(years, gsub("\\D", "", year))
+        years <- append(years, as.integer(gsub("\\D", "", year)))
 
-        scores <- append(scores, (html_node(movie, ".ratings-imdb-rating strong") %>% html_text))
+        score <- (html_node(movie, ".ratings-imdb-rating strong") %>% html_text)
+        if (!is.na(score)) { score <- as.integer(as.double(score) * 10) }
+        scores <- append(scores, as.integer(score))
 
         runtimes <- append(runtimes, gsub("\\D", "", (html_node(movie, ".runtime") %>% html_text)))
 
-        metaScores <- append(metaScores, (html_node(movie, ".metascore") %>% html_text))
+        metaScore <- html_node(movie, ".metascore") %>% html_text
+        metaScores <- append(metaScores, as.integer(metaScore))
 
-        grossProfits <- append(grossProfits, (html_node(movie, "[name=nv]") %>% html_text))
-
+        grossProfit <- html_node(movie, "[name=nv]") %>% html_text
+        grossProfits <- append(grossProfits, as.double(grossProfit))
+        
         genres <- append(genres, (html_node(movie, ".genre") %>% html_text))
     }
-
     return(data.frame(title = titles, release_year = years, imdb_score = scores, meta_score = metaScores, gross_profits = grossProfits))
 }
 
+# Parses a single page from IMDB, returns the data frame from the parseMovies Function
+parseImbdPage <- function(url) {
+    options(stringsAsFactors = FALSE)
 
-movies <- data.frame(title = character(), release_year = character(), imdb_score = character(), meta_score = character(), gross_profits = character())
-
-
-
-is_error <- FALSE
-i <- 1
-
-
-while (!is_error) {
-    print(paste("Reading page: ", i, sep = ""))
-    imdbURL <- url(paste("https://www.imdb.com/search/title?year=2018,2018&title_type=feature&sort=moviemeter,asc&page=", i, "&ref_=adv_nxt", sep = ""))
-
-    result = tryCatch({
-        page_html <- read_html(imdbURL)
+    tryCatch({
+        page_html <- read_html(url)
         error <- html_nodes(page_html, ".error_code") %>% html_text
 
         if (length(error) > 0) {
-            is_error <<- TRUE;
-            break;
+            return(NA);
         }
 
         items <- html_nodes(page_html, ".lister-item")
-        len <- length(items);
-        movies <- rbind(movies, parseMovies(items))
-    }, warning = function(w) {
-        is_error <<- TRUE
-    }, error = function(e) {
-        is_error <<- TRUE
-    }, finally = {
-        i <- i + 1
-    })
+        len <- length(items)
+        return(suppressWarnings(parseMovies(items)))
+    },
+    warning = function(w) {
+        return(NA)
+    },
+    error = function(e) {
+        return(NA)
+    },
+    finally = { }
+    )
+}
+
+combineListToDF <- function(x, ...) {
+    rbind(x, ...)
 }
 
 
-movies
+#setup parallel backend to use many processors
+cores = detectCores()
 
+#not to overload your computer
+cl <- makeCluster(cores[1] - 1)
+registerDoParallel(cl)
+
+movies <- data.frame(title = character(), release_year = integer(), imdb_score = integer(), meta_score = integer(), gross_profits = double())
+i <- 1
+
+movies <- foreach(i = 1:201, .combine = combineListToDF, .packages = c("rvest", "magrittr")) %do% {
+
+    imdbURL <- url(paste("https://www.imdb.com/search/title?year=2018,2018&title_type=feature&sort=moviemeter,asc&page=", i, "&ref_=adv_nxt", sep = ""))
+
+    parseImbdPage(imdbURL)
+}
+
+summary(movies)

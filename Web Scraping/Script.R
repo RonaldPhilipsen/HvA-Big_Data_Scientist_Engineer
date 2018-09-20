b6ef2caf381a1e4e73a556130679be113fd380be
Ronald Philipsen <Ronald@solid-optics.com>
2018-09-20 16:23:19 +0200

    pull stuff from multiple pages

--- Web Scraping/Script.R
+++ Web Scraping/Script.R
@@ -10,7 +10,7 @@ parseMovies <- function(movies) {
     runtimes <- c()
     metaScores <- c()
     grossProfits <- c()
-
+    genres <- c()
 
     for (movie in movies) {
         titles <- append(titles, (html_node(movie, ".lister-item-header a") %>% html_text))
@@ -22,20 +22,50 @@ parseMovies <- function(movies) {
 
         runtimes <- append(runtimes, gsub("\\D", "", (html_node(movie, ".runtime") %>% html_text)))
 
-        metaScores <- append(metaScores,(html_node(movie, ".metascore") %>% html_text))
+        metaScores <- append(metaScores, (html_node(movie, ".metascore") %>% html_text))
 
         grossProfits <- append(grossProfits, (html_node(movie, "[name=nv]") %>% html_text))
+
+        genres <- append(genres, (html_node(movie, ".genre") %>% html_text))
     }
 
-    return(data.frame(title = titles, release_year = years, imdb_score= scores, meta_score = metaScores, gross_profits = grossProfits))
+    return(data.frame(title = titles, release_year = years, imdb_score = scores, meta_score = metaScores, gross_profits = grossProfits))
 }
 
-imdbURL <- url("https://www.imdb.com/search/title?year=2018,2018&title_type=feature&sort=moviemeter,asc")
-page_html <- read_html(imdbURL)
-items <- html_nodes(page_html, ".lister-item")
-len <- length(items);
 
-movies <- parseMovies(items)
+movies <- data.frame(title = character(), release_year = character(), imdb_score = character(), meta_score = character(), gross_profits = character())
+
+
+
+is_error <- FALSE
+i <- 1
+
+
+while (!is_error) {
+    print(paste("Reading page: ", i, sep = ""))
+    imdbURL <- url(paste("https://www.imdb.com/search/title?year=2018,2018&title_type=feature&sort=moviemeter,asc&page=", i, "&ref_=adv_nxt", sep = ""))
+
+    result = tryCatch({
+        page_html <- read_html(imdbURL)
+        error <- html_nodes(page_html, ".error_code") %>% html_text
+
+        if (length(error) > 0) {
+            is_error <<- TRUE;
+            break;
+        }
+
+        items <- html_nodes(page_html, ".lister-item")
+        len <- length(items);
+        movies <- rbind(movies, parseMovies(items))
+    }, warning = function(w) {
+        is_error <<- TRUE
+    }, error = function(e) {
+        is_error <<- TRUE
+    }, finally = {
+        i <- i + 1
+    })
+}
+
 
 movies
 

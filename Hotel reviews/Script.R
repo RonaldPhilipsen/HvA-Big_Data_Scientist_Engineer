4f45ea2ae1ea5764e9dfab674f4dfde82f858a27
Ronald Philipsen <Mail@rphilipsen.nl>
2018-10-08 21:13:24 +0200

    in de les meuk

--- Hotel reviews/Script.R
+++ Hotel reviews/Script.R
@@ -4,36 +4,24 @@ library(dplyr)
 library(dtplyr)
 library(tm)
 library(mlr)
-library(parallelMap)
 library(e1071)
 
-finn <- get_sentiments("afinn")
-bing <- get_sentiments("bing")
-nrc <- get_sentiments("nrc")
-undesirable_words <- c("No positive", "No Negative", "AND", "it", "in")
-stop_words <- stopwords(kind = "en")
-
-max_rows <- 1000
-min_rows <- 1
+undesirable_words <- c("no positive", "no negative", "and", "it", "in", "hotel")
 
 # Regex pattern for removing stop words
 stop_pattern <- paste0("\\b(", paste0(stopwords("en"), collapse = "|"), ")\\b")
 undesirable_pattern <- paste0("\\b(", paste0(undesirable_words, collapse = "|"), ")\\b")
 
 df <- read.csv("Hotel_Reviews.csv", stringsAsFactors = FALSE)
-
-#df$Combined_Review <- paste(df$Positive_Review, df$Negative_Review)
 combined <- group_by(df, Hotel_Name)[min_rows:max_rows,]
-
-#combined$ID <- seq.int(nrow(combined))
 
 pr <- as.data.frame(t(rbind(Review = combined$Positive_Review, Consensus = +1)))
 nr <- as.data.frame(t(rbind(Review = combined$Negative_Review, Consensus = -1)))
 all_reviews <- rbind(pr, nr)
-all_reviews <- all_reviews[sample.int(nrow(all_reviews)),]
-
+all_reviews <- all_reviews[sample(nrow(all_reviews)),][1:1000,]
 
 all_reviews$Review <- all_reviews$Review %>%
+    tolower() %>%
     gsub(pattern = "[a-zA-Z]*([0-9]{3,})[a-zA-Z0-9]* ?", replacement = "") %>%
     gsub(pattern = "\\d+ ", replacement = "") %>%
     gsub(pattern = "[[:punct:]]", replacement = "") %>%
@@ -43,22 +31,17 @@ all_reviews$Review <- all_reviews$Review %>%
     gsub(pattern = " {2,}", replacement = " ") %>%
     trimws()
 
+
 training_set <- all_reviews[1:20,]
-
-
-matrixReviews = RTextTools::create_matrix(all_reviews[min_rows:max_rows, 1], language = "english",
-                      removeStopwords = FALSE, removeNumbers = TRUE,
-                      stemWords = FALSE)
+matrixReviews = RTextTools::create_matrix(all_reviews[, 1],
+                                          language = "english",
+                                          removeStopwords = FALSE,
+                                          removeNumbers = FALSE,
+                                          stemWords = FALSE)
 maReviews = as.matrix(matrixReviews)
 
 
-classifier = naiveBayes(maReviews[1:25,], as.factor(all_reviews[1:25, 2]))
-predicted = predict(classifier, maReviews[26:100,])
-print(table(all_reviews[26:100, 2], predicted))
-RTextTools::recall_accuracy(all_reviews[26:100, 2], predicted)
-
-
-#docs <- Corpus(VectorSource(combined$Positive_Review)) %>%
-#    tm_map(removePunctuation) %>%
-#    tm_map(removeNumbers) %>%
-#    tm_map(cleanData)
+classifier = naiveBayes(maReviews[1:1000,], as.factor(all_reviews[1:1000,2]))
+predicted = predict(classifier, maReviews[101:200,])
+print(table(all_reviews[101:200, 2], predicted))
+RTextTools::recall_accuracy(all_reviews[101:200, 2], predicted)

e355347e63aa0d9ae7b8cf8328ef4dbbcd66d792
Ronald Philipsen <Mail@rphilipsen.nl>
2018-11-03 16:40:13 +0100

    did stuff for hand written reviews

--- Hotel reviews/Script.R
+++ Hotel reviews/Script.R
@@ -1,15 +1,18 @@
-#install_github("ronald245/RTextTools", subdir = "RTextTools")
+library(devtools)
+install_github("ronald245/RTextTools", subdir = "RTextTools")
+library("RtextTools")
+
 required_packages <- c("magrittr", "tm", "mlr", "e1071", "dplyr", "kernlab", "lubridate", "dtplyr",
                        "readr", "ggplot2", "tidytext", "stringr", "tidyr", "scales", "broom",
-                       "SnowballC", "wordcloud", "reshape2", "RTextTools", "rvest",
-                       "tibble", "devtools", "DBI", "httr", "RMySQL")
+                       "SnowballC", "wordcloud", "reshape2", "rvest",
+                       "tibble", "DBI", "httr", "RMySQL")
 extra_scripts <- c("Tools.R", "Sentiment.R", "Classifiers.R", "CleanData.R", "Scraper.R")
 
 x <- lapply(required_packages, require, character.only = TRUE)
 x <- lapply(extra_scripts, source)
 
 database <- "HotelReviews"
-database.ip <- "10.0.0.168"
+database.ip <- "localhost"
 database.user <- "username"
 database.password <- "password"
 
@@ -101,6 +104,52 @@ for (i in c(1:nrow(test))) {
     test$calculatedConsensus[i] = consensus
 }
 
+
+analytics <- create_analytics(container, results)
+summary(analytics)
+recall_accuracy(test$Consensus, results$LOGITBOOST_LABEL)
+recall_accuracy(test$Consensus, results$GLMNET_LABEL)
+recall_accuracy(test$Consensus, results$FORESTS_LABEL)
+recall_accuracy(test$Consensus, results$SVM_LABEL)
+
+
+
+review.1 <- read_file("review5.csv")
+review.2 <- read_file("review6.csv")
+reviews.1 <- tibble(review_body = review.1, " ", Consensus = 0)
+reviews.2 <- tibble(review_body = review.2, " ", Consensus = 0)
+reviews <- rbind(reviews.1,reviews.2)
+
+new_matrix <- create_matrix(reviews[, "review_body"],
+                            language = "english",
+                            removeNumbers = TRUE,
+                            stemWords = TRUE,
+                            removeSparseTerms = .99,
+                            weighting = weightTfIdf,
+                            originalMatrix = doc_matrix)
+
+container <- create_container(new_matrix,
+                              reviews$Consensus,
+                              testSize = 1:nrow(reviews),
+                              virgin = TRUE)
+
+results <- classify_models(container, models)
+
+reviews$LOGITBOOST_LABEL = results$LOGITBOOST_LABEL
+reviews$GLMNET_LABEL = results$GLMNET_LABEL
+reviews$FORESTS_LABEL = results$FORESTS_LABEL
+reviews$SVM_LABEL = results$SVM_LABEL
+reviews$calculatedConsensus <- 0
+
+for (i in c(1:nrow(reviews))) {
+    consensus = 0
+    consensus <- if (results$LOGITBOOST_LABEL[i] == +1) consensus + 1 else consensus - 1
+    consensus <- if (results$GLMNET_LABEL[i] == +1) consensus + 1 else consensus - 1
+    consensus <- if (results$FORESTS_LABEL[i] == +1) consensus + 1 else consensus - 1
+    consensus <- if (results$SVM_LABEL[i] == +1) consensus + 1 else consensus - 1
+
+    test$calculatedConsensus[i] = consensus
+}
 
 analytics <- create_analytics(container, results)
 summary(analytics)
